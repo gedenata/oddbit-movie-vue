479fd78be95a41049476a9b938e2d80b96193bce
gedenata <igedetirtanata@gmail.com>
2020-09-07 15:40:16 +0800

    [add] baseUrl production

--- src/App.vue
+++ src/App.vue
@@ -8,12 +8,40 @@ router-view(key="$route.fullPath") -->
   <div id="app">Hello</div>
 </template>
 
-<script></script>
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  components: {},
+  computed: {
+    ...mapGetters(["main_spinner", "show_error", "app_padding"])
+  },
+  created() {
+    this.$store.dispatch("GET_CONFIGURATION");
+  },
+  mounted() {}
+};
+</script>
 
 <style lang="scss">
 #app {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+}
+
+.container {
+  position: relative;
+  &--main {
+    padding-top: 20px;
+  }
+}
+
+.disable-hover {
+  pointer-events: none;
+}
+
+ul {
+  box-shadow: none;
 }
 </style>
