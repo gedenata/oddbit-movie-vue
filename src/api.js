5a8718e5f47b67025e3ee3b4c3f21e7651905214
gedenata <igedetirtanata@gmail.com>
2020-09-07 11:48:46 +0800

    [add] axios and vue-router, api reference

---
+++ src/api.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
+  params: {
+    language: "en-US",
+    api_key: "1b869b3ccf57d089047ded4b1de007b8"
+  },
+  validateStatus: function(status) {
+    return status >= 200 && status < 300;
+  }
+});
+
+export default api;
