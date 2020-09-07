b45a20f9ec96bdf8748cc25b312db2d8c7a6cda1
gedenata <igedetirtanata@gmail.com>
2020-09-07 18:40:33 +0800

    [add] view page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,105 @@
-<template lang="pug">
-  
+<template>
+  <section>
+    <ul class="popular row m-0">
+      <li class="col-6 col-sm-6 col-md-4 col-lg-3" v-for="(item, index) in popularItems">
+        <popular :item="item" :key="item.id"></popular>
+      </li>
+    </ul>
+    <spinner :is-loading="popularLength" :is-relative="true"></spinner>
+  </section>
 </template>
 
 <script>
+import _ from "lodash";
+import { mapGetters } from "vuex";
+
+import Popular from "~/components/Popular";
+import Spinner from "~/components/Spinner";
+import Sorting from "~/components/Sorting";
+
 export default {
-  //
+  name: "home",
+  components: {
+    Popular,
+    Spinner,
+    Sorting
+  },
+  data: () => ({
+    enableLoading: true
+  }),
+  computed: {
+    ...mapGetters(["popular", "page", "sort_options"]),
+    popularItems() {
+      return this.popular.filter(
+        item => item.backdrop_path !== null || item.backdrop_path !== undefined
+      );
+    },
+    popularLength() {
+      return this.popular.length > 1;
+    }
+  },
+  methods: {
+    scroll() {
+      const self = this;
+      window.addEventListener("scroll", () => {
+        let bottomOfWindow =
+          document.documentElement.scrollTop + window.innerHeight ===
+          document.documentElement.offsetHeight;
+        if (bottomOfWindow && this.enableLoading) {
+          this.nextPage(self);
+        }
+      });
+    },
+    getFilms() {
+      this.$store.dispatch("GET_FILMS", {
+        url: "/discover/movie",
+        key: "popular",
+        params: {
+          ...this.sort_options
+        }
+      });
+    },
+    nextPage: _.debounce(self => {
+      self.getFilms();
+    }, 600)
+  },
+  created() {
+    this.getFilms();
+  },
+  mounted() {
+    this.scroll();
+  }
 };
 </script>
 
 <style lang="scss" scoped>
-//
+.popular {
+  position: relative;
+  padding: 0;
+  padding-bottom: 100px;
+  min-height: 666px;
+
+  .item {
+    height: 100%;
+  }
+}
+
+.pupular > li {
+  list-style: none;
+  padding: 0;
+  margin: 8px 0;
+}
+
+.release-date {
+  top: 10px;
+  left: 10px;
+  padding: 2px;
+  color: #ffffff;
+  position: absolute;
+  transform: rotate(-45deg);
+  transform-origin: center bottom;
+  display: inline-block;
+  background-color: #696969;
+  border: 1px solid #ffffff;
+}
 </style>
--- src/views/Movie.vue
+++ src/views/Movie.vue
@@ -1,13 +1,201 @@
 <template lang="pug">
-  
+  <div class="details">
+    <h2>{{ details.title }}</h2>
+    <div class="info">
+      <div class="info">
+        <dl class="vote-average">
+          <dt>Vote average</dt>
+          <dd>{{ details.vote_average }}</dd>
+        </dl>
+        <dl class="vote-count">
+          <dt>Vote count</dt>
+          <dd>{{ details.vote_count }}</dd>
+        </dl>
+        <dl class="vote-count">
+          <dt>Popularity</dt>
+          <dd>{{ details.popularity }}</dd>
+        </dl>
+        <dl class="details-release-date">
+          <dt>Release date</dt>
+          <dd>{{ details.release_date | formateDate }}</dd>
+        </dl>
+      </div>
+    </div>
+    <template v-if="details.backdrop_path == null"></template>
+    <template v-else>
+      <poster :src="details.backdrop_path" data-image-type="poster"></poster>
+    </template>
+    <ul>
+      <li>
+        <p>{{ details.overview }}</p>
+      </li>
+    </ul>
+    <ul class="genres-list" v-show="details.genres">
+      <li class="title"><b>Genres</b></li>
+      <li v-for="item in details.genres">
+        <span class="tag">{{ item.name }}</span>
+      </li>
+    </ul>
+    <similar :id="id"></similar>
+    <ul class="production-list" v-show="details.production_companies">
+      <li class="title"><b>Production companies</b></li>
+      <li v-for="item in details.production_companies">
+        <template v-if="item.logo_path">
+          <poster class="thumb" :src="item.logo_path" size="xs" :fade-on-hover="false"></poster>
+        </template>
+        <template v-else>
+          <div class="name">{{ item.name }}</div>
+        </template>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
+import filters from "./../filters";
+
+import Poster from "~/components/Poster";
+import Similar from "~/components/Similar";
+
 export default {
-  //
+  name: "movie",
+  props: {
+    id: {
+      type: [Number, String],
+      required: true
+    }
+  },
+  components: {
+    Poster,
+    Similar
+  },
+  watch: {
+    id() {
+      this.$store.dispatch("GET_DETAILS", this.id);
+    }
+  },
+  filters,
+  computed: {
+    ...mapState(["details"])
+  },
+  created() {
+    this.$store.dispatch("GET_DETAILS", this.id);
+  },
+  mounted() {
+    this.$store.dispatch("TOGGLE", { main_spinner: true });
+  }
 };
 </script>
 
 <style lang="scss" scoped>
-//
+.details {
+  h2 {
+    padding: 0;
+    font-size: 32px;
+    @media (min-width: 992px) {
+      padding-top: 16px;
+      padding-bottom: 16px;
+    }
+  }
+  margin-bottom: 100px;
+}
+
+.genres-list,
+.production-list,
+.description-list {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  position: relative;
+  padding: 0;
+
+  > li {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    text-align: center;
+    position: relative;
+  }
+
+  > .title {
+    font-size: 16px;
+    text-align: center;
+  }
+
+  > .name {
+    font-size: 14px;
+  }
+}
+
+.genres-list {
+  li {
+    margin: 10px;
+  }
+}
+
+.production-list {
+  li {
+    padding: 15px;
+  }
+}
+
+.description-list p {
+  font-size: 16px;
+  text-align: left;
+  padding: 16px 0 0;
+  margin-bottom: 0;
+  @media (min-width: 992px) {
+    font-size: 22px;
+  }
+}
+
+.info {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+
+  dl {
+    padding: 0;
+    color: #636e72;
+  }
+
+  dt {
+    padding: 0 16px;
+    font-size: 16px;
+  }
+
+  dd {
+    font-size: 24px;
+    font-weight: 700;
+    padding: 0 16px;
+    display: inline-block;
+    letter-spacing: 2px;
+  }
+}
+
+.production-list {
+  &:before {
+    background: #ffffff;
+  }
+}
+
+.movie-similar {
+  margin-bottom: 20px;
+}
+
+.tag {
+  color: #ffffff;
+  padding: 10px 20px;
+  display: inline-block;
+  background-color: #636e72;
+}
+
+.thumb {
+  width: 60px;
+  .background-image {
+    background-color: transparent;
+  }
+}
 </style>
