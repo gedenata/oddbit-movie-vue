c300a898f6037e92d40ccf799c976abac07c89c3
gedenata <igedetirtanata@gmail.com>
2020-09-08 09:10:23 +0800

    [fix] typo

--- src/api.js
+++ src/api.js
@@ -3,8 +3,7 @@ import axios from "axios";
 const Http = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
-    // api_key: "1b869b3ccf57d089047ded4b1de007b8"
-    api_key: "af9c62f06479e409fbc8b1ee21126a96"
+    api_key: "1b869b3ccf57d089047ded4b1de007b8"
   },
   validateStatus: function(status) {
     return status >= 200 && status < 300;
